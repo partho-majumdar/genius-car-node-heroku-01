1c5bf40b5dff4f969de215629dc01519f9314dc0
heipartho05 <heipartho72@gmail.com>
2021-10-28 20:30:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId; // for 3 and dynamic route
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// user name : genius-car-mechanics
+// password : zYf49AKVUGBd28vz
+
+/* Middleware */
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y2kiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("carMechanic");
+    const serviceCollection = database.collection("services");
+
+    //GET API 02
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //GET SINGLE SERVICE 03
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.json(service);
+    });
+
+    // POST API 01
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+
+      const result = await serviceCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
+
+    //DELETE API 04
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Genius Car!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
